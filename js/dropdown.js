c4a7b86cfd16b1301df4d9679388193f02949ecb
apstudy <143913641+apstudy@users.noreply.github.com>
2024-07-18 01:48:35 -0700

    Updates

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,12 +2,6 @@ const dropdownHeaders = document.getElementsByClassName("dropdown-header");
 
 for (i = 0; i < dropdownHeaders.length; i++) {
   dropdownHeaders[i].addEventListener("click", (e) => {
-    const header = e.currentTarget;
-    header.classList.toggle("open");
-    if (header.classList.contains("open")) {
-      header.textContent = header.textContent.replace("⯈","⯆");
-    } else {
-      header.textContent = header.textContent.replace("⯆","⯈");
-    }
+    e.currentTarget.parentNode.classList.toggle("open");
   })
 }
