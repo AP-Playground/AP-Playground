cbe93d289556ac060f304f305ad446c0aab19cbe
apstudy <143913641+apstudy@users.noreply.github.com>
2025-04-16 01:39:47 -0700

    Creating an APAH review game

---
+++ ap-art-history/games/index.js
@@ -0,0 +1,21 @@
+const unitOptions = Array.from(document.querySelectorAll(".unit-option"));
+const gameOptions = Array.from(document.querySelectorAll(".game-option"));
+const identifierOptions = Array.from(document.querySelectorAll(".identifier-option"));
+
+document.querySelector("#game-options").addEventListener("submit", function (e) {
+  if (!unitOptions.some((option => option.checked))) {
+    e.preventDefault();
+    alert("Please select at least one unit!");
+    return;
+  }
+  if (!gameOptions.some((option => option.checked))) {
+    e.preventDefault();
+    alert("Please select at least one game!");
+    return;
+  }
+  if (!identifierOptions.some((option => option.checked))) {
+    e.preventDefault();
+    alert("Please select an identifier!");
+    return;
+  }
+})
