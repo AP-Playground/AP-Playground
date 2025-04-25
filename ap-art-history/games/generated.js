48b6e60475458c1bcc18e65cd4dc95800cbe10f8
apstudy <143913641+apstudy@users.noreply.github.com>
2025-04-24 17:30:17 -0700

    Updated APAH practice game

--- ap-art-history/games/generated.js
+++ ap-art-history/games/generated.js
@@ -50,19 +50,11 @@ function loadGame() {
     }
   }
 
-  if (games.includes("date")) {
-    gameDate.style.display = "block";
-  } else {
-    gameDate.style.display = "none";
-  }
-  if (games.includes("artist")) {}
-  if (games.includes("location")) {
-    gameLocation.style.display = "block";
-  } else {
-    gameLocation.style.display = "none";
-  }
-  if (games.includes("style")) {}
-
+  gameDate.style.display = games.includes("date") ? "grid" : "none";
+  gameArtist.style.display = games.includes("artist") ? "grid" : "none";
+  gameLocation.style.display = games.includes("location") ? "block" : "none";
+  gameStyle.style.display = games.includes("style") ? "grid" : "none";
+  
   loading.style.display = "none";
   gameContainer.style.display = "grid";
   loadCard(cards[0]);
@@ -70,6 +62,12 @@ function loadGame() {
 
 
 function loadCard(card) {
+
+  if (cards[1] && typeof cards[1].image === "string") {
+    const preload = new Image();
+    preload.src = cards[1].image;
+  }
+
   if (identifier !== "title") {
     identifierImage.src = chooseOne(card.image);
   }
@@ -92,8 +90,20 @@ function loadCard(card) {
     locationCorrect.style.display = "none";
     locationCorrect.style.top = ((90-card["location-coordinate"][1])/180*100) + "%";
     locationCorrect.style.left = ((card["location-coordinate"][0]+180)/360*100) + "%";
+    locationMap.style.cursor = "pointer";
   }
-
+  if (games.includes("artist")) {
+    artistInput.classList.remove("correct", "almost-correct", "incorrect");
+    artistInput.value = "";
+    artistInput.disabled = false;
+    artistFeedback.innerText = "";
+  }
+  if (games.includes("style")) {
+    styleInput.classList.remove("correct", "almost-correct", "incorrect");
+    styleInput.value = "";
+    styleInput.disabled = false;
+    styleFeedback.innerText = "";
+  }
 }
 
 
@@ -107,58 +117,148 @@ dateToggle.addEventListener("click", () => {
   dateToggle.innerText = dateStatus;
 });
 
+
+const artistInput = document.getElementById("game-artist-input");
+const artistFeedback = document.getElementById("game-artist-feedback");
+
+const styleInput = document.getElementById("game-style-input");
+const styleFeedback = document.getElementById("game-style-feedback");
+
 function checkAnswer() {
-  if (games.includes("date")) {
-    let date = Number(dateInput.value);
-    if (dateStatus === "BCE") {
-      date = -date;
-    }
+  if (games.includes("date")) checkDate();
+  
+  if (games.includes("location")) checkLocation();
 
-    let dateBounds = cards[0]["date-range"];
-    let dateNum = cards[0].dateNum;
-    if (typeof dateNum === "number" && date === dateNum) {
-      dateInput.classList.add("correct");
-      score += 1;
-    } else if (typeof dateNum !== "number" && date >= dateNum[0] && date <= dateNum[1]) {
-      dateInput.classList.add("correct");
-      score += 1;
-      dateFeedback.innerText = cards[0].date;
-    } else if (date >= dateBounds[0] && date <= dateBounds[1]){
-      dateInput.classList.add("almost-correct");
-      score += 1-2*Math.abs(dateNum-date)/(dateBounds[1]-dateBounds[0]);
-      dateFeedback.innerText = cards[0].date;
-    } else {
-      dateInput.classList.add("incorrect");
-      dateFeedback.innerText = cards[0].date;
-    }
+  if (games.includes("artist")) checkArtist();
 
-    dateInput.disabled = true;
-    dateToggle.disabled = true;
+  if (games.includes("style")) checkStyle();
+}
+
+function checkArtist() {
+  if (artistInput.value === "") {
+    artistInput.classList.add("incorrect");
+    artistFeedback.innerText = cards[0].artist[0];
+    return;
+  }
+  let artist = artistInput.value;
+  let artistCorrect = cards[0].artist;
+
+  let closest = 10000;
+  let closestIndex = -1;
+  styleCorrect.forEach((s,i) => {
+    let distance = levenshtein(style, s);
+    if (distance < closest) {
+      closest = distance;
+      closestIndex = i;
+    }
+  })
+
+  if (closest === 0) {
+    artistInput.classList.add("correct");
+    score += 1;
+  } else if (closest < 3) {
+    styleInput.classList.add("almost-correct");
+    score += 1-closest/10;
+  } else {
+    styleInput.classList.add("incorrect");
   }
   
-  if (games.includes("location")) {
-    if (locationSelection.style.display === "none") {
-      locationFeedback.innerText = cards[0].location;
-    } else {
-      locationCorrect.style.display = "flex";
-      let locSelectionX = Number.parseFloat(locationSelection.style.left)*3.6-180;
-      let locSelectionY = 90-Number.parseFloat(locationSelection.style.top)*1.8;
-      let locCorrectX = cards[0]["location-coordinate"][0];
-      let locCorrectY = cards[0]["location-coordinate"][1];
-      locSelectionX *= Math.PI/180;
-      locSelectionY *= Math.PI/180;
-      locCorrectX *= Math.PI/180;
-      locCorrectY *= Math.PI/180;
-      let radius = 3963.1;
-      let calc = 1-Math.cos(locCorrectY)*Math.cos(locSelectionY)*Math.cos(locCorrectX-locSelectionX)-Math.sin(locCorrectY)*Math.sin(locSelectionY);
-      let distance = 2*Math.asin(Math.sqrt(calc)/2)*radius;
+  artistFeedback.innerText = artistCorrect[0];
+}
 
-      locationFeedback.innerText = Math.round(distance) + " miles away; " + cards[0].location;
-      if (distance < 150) {
-        score += 1
-      } else if (distance < 500) {
-        score += 1-(distance-150)/350;
-      }
+
+function checkStyle() {
+  if (styleInput.value === "") {
+    styleInput.classList.add("incorrect");
+    styleFeedback.innerText = cards[0].style.join(", ");
+    return;
+  }
+  let style = styleInput.value;
+  let styleCorrect = cards[0].style;
+
+  let closest = 10000;
+  let closestIndex = -1;
+  styleCorrect.forEach((s,i) => {
+    let distance = levenshtein(style, s);
+    if (distance < closest) {
+      closest = distance;
+      closestIndex = i;
+    }
+  })
+
+  if (closest === 0) {
+    styleInput.classList.add("correct");
+    score += 1;
+  } else if (closest < 3) {
+    styleInput.classList.add("almost-correct");
+    score += 1-closest/10;
+  } else {
+    styleInput.classList.add("incorrect");
+  }
+
+  styleFeedback.innerText = styleCorrect.join(", ");
+}
+
+
+function checkDate() {
+  if (dateInput.value === "") {
+    dateInput.classList.add("incorrect");
+    dateFeedback.innerText = cards[0].date;
+    return;
+  }
+
+  let date = Number(dateInput.value);
+  if (dateStatus === "BCE") {
+    date = -date;
+  }
+
+  let dateBounds = cards[0]["date-range"];
+  let dateNum = cards[0].dateNum;
+  if (typeof dateNum === "number" && date === dateNum) {
+    dateInput.classList.add("correct");
+    score += 1;
+  } else if (typeof dateNum !== "number" && date >= dateNum[0] && date <= dateNum[1]) {
+    dateInput.classList.add("correct");
+    score += 1;
+    dateFeedback.innerText = cards[0].date;
+  } else if (date >= dateBounds[0] && date <= dateBounds[1]){
+    dateInput.classList.add("almost-correct");
+    score += 1-2*Math.abs(dateNum-date)/(dateBounds[1]-dateBounds[0]);
+    dateFeedback.innerText = cards[0].date;
+  } else {
+    dateInput.classList.add("incorrect");
+    dateFeedback.innerText = cards[0].date;
+  }
+
+  dateInput.disabled = true;
+  dateToggle.disabled = true;
+}
+
+
+function checkLocation() {
+  locationMap.style.cursor = "default";
+
+  if (locationSelection.style.display === "none") {
+    locationFeedback.innerText = cards[0].location;
+  } else {
+    locationCorrect.style.display = "flex";
+    let locSelectionX = Number.parseFloat(locationSelection.style.left)*3.6-180;
+    let locSelectionY = 90-Number.parseFloat(locationSelection.style.top)*1.8;
+    let locCorrectX = cards[0]["location-coordinate"][0];
+    let locCorrectY = cards[0]["location-coordinate"][1];
+    locSelectionX *= Math.PI/180;
+    locSelectionY *= Math.PI/180;
+    locCorrectX *= Math.PI/180;
+    locCorrectY *= Math.PI/180;
+    let radius = 3963.1;
+    let calc = 1-Math.cos(locCorrectY)*Math.cos(locSelectionY)*Math.cos(locCorrectX-locSelectionX)-Math.sin(locCorrectY)*Math.sin(locSelectionY);
+    let distance = 2*Math.asin(Math.sqrt(calc)/2)*radius;
+
+    locationFeedback.innerText = (new Intl.NumberFormat().format(Math.round(distance))) + " miles away; " + cards[0].location;
+    if (distance < 150) {
+      score += 1
+    } else if (distance < 500) {
+      score += 1-(distance-150)/350;
     }
   }
 }
@@ -169,6 +269,9 @@ const locationCorrect = document.getElementById("game-location-correct");
 const locationMap = document.getElementById("game-location-map");
 const locationFeedback = document.getElementById("game-location-feedback");
 locationMap.addEventListener("click", (event) => {
+  if (locationCorrect.style.display !== "none") {
+    return;
+  }
   const rect = locationMap.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
@@ -216,4 +319,28 @@ function chooseOne(i) {
     return i;
   }
   return i[Math.floor(Math.random() * i.length)];
+}
+
+function levenshtein(input, target) {
+  input = input.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  target = target.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  let v0 = [];
+  let v1 = new Array(target.length + 1);
+
+  for (let i = 0; i < target.length + 1; i++) {
+    v0.push(i);
+  }
+
+  for (let i = 0; i < input.length; i++) {
+    v1[0] = i + 1;
+    for (let j = 0; j < target.length; j++) {
+      v1[j+1] = Math.min(
+        v0[j+1] + 1, // deletion
+        v1[j] + 1,   // insertion
+        v0[j] + (input[i] === target[j] ? 0 : 1) // substitution
+      );
+    }
+    [v0, v1] = [v1, v0]; // swap
+  }
+  return v0[target.length];
 }
