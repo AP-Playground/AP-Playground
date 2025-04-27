beadcd8d7fdba9d62de717ce9d428a1eed6309f1
apstudy <143913641+apstudy@users.noreply.github.com>
2025-04-27 02:35:30 -0700

    Simplified code

--- ap-art-history/games/index.js
+++ ap-art-history/games/index.js
@@ -3,17 +3,17 @@ const gameOptions = Array.from(document.querySelectorAll(".game-option"));
 const identifierOptions = Array.from(document.querySelectorAll(".identifier-option"));
 
 document.querySelector("#game-options").addEventListener("submit", function (e) {
-  if (!unitOptions.some((option => option.checked))) {
+  if (!unitOptions.some(option => option.checked)) {
     e.preventDefault();
     alert("Please select at least one unit!");
     return;
   }
-  if (!gameOptions.some((option => option.checked))) {
+  if (!gameOptions.some(option => option.checked)) {
     e.preventDefault();
     alert("Please select at least one game!");
     return;
   }
-  if (!identifierOptions.some((option => option.checked))) {
+  if (!identifierOptions.some(option => option.checked)) {
     e.preventDefault();
     alert("Please select an identifier!");
     return;
