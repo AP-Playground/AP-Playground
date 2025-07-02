1ae05b9d0b288a369d95192d6249596eae53914b
apstudy <143913641+apstudy@users.noreply.github.com>
2025-07-01 22:06:47 -0700

    Update generate.js

--- scripts/generate.js
+++ scripts/generate.js
@@ -32,13 +32,14 @@ pages.forEach(({ filename, content }) => {
 // copy icons from src/icons to public/icons
 const icons = fs.readdirSync(path.resolve(__dirname, '..', 'src/icons'));
 icons.forEach(icon => {
+  const srcPath = path.resolve(__dirname, "..", "src", "icons", icon)
   const destPath = path.resolve(outDir, "icons", icon);
   const destDir = path.dirname(destPath);
 
   // Ensure destination directory exists
   fs.mkdirSync(destDir, { recursive: true });
 
-  fs.copyFileSync(icon, destDir)
+  fs.copyFileSync(srcPath, destDir)
 })
 
 
