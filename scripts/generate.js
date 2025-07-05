77b05c3ee69b80313e565512ccad1aed503c2390
apstudy <143913641+apstudy@users.noreply.github.com>
2025-07-04 17:06:14 -0700

    Improving side-nav

--- scripts/generate.js
+++ scripts/generate.js
@@ -88,24 +88,24 @@ function genLesson(filename) {
   const pagePath = data.slug.split("/")
 
   page = page.replaceAll("{{course.title}}", navData.title)
+  page = page.replaceAll("{{course.slug}}", pagePath[0]);
+  page = page.replaceAll("{{unit.slug}}", pagePath[1]);
+  page = page.replaceAll("{{lesson.slug}}", pagePath[2]);
 
   let navText = "";
   navData.units.forEach(unit => {
-    navText += `<li><a href="/${navData.course}/${unit.slug}" class="item">${unit.prefix}: ${unit.title}</a></li>`
+    navText += `<li class="item"><a href="/${navData.course}/${unit.slug}">${unit.prefix}: ${unit.title}</a></li>`
     if (unit.slug === pagePath[1]) {
       unit.lessons.forEach(lesson => {
         if (lesson.slug === pagePath[2]) {
-          navText += `<li><a class="sub-item">${lesson.prefix}: ${lesson.title}</a></li>`;
+          navText += `<li class="sub-item"><a>${lesson.prefix}: ${lesson.title}</a></li>`;
           
           page = page.replaceAll("{{page.title}}", lesson.prefix + ": " + lesson.title);
 
-          page = page.replaceAll("{{course.slug}}", navData.course);
-          page = page.replaceAll("{{unit.slug}}", unit.slug);
-          page = page.replaceAll("{{lesson.slug}}", lesson.slug);
           page = page.replaceAll("{{unit.title}}", unit.prefix + ": " + unit.title);
           page = page.replaceAll("{{lesson.title}}", lesson.prefix + ": " + lesson.title);
         } else {
-          navText += `<li><a href="/${navData.course}/${unit.slug}/${lesson.slug}" class="sub-item">${lesson.prefix}: ${lesson.title}</a></li>`
+          navText += `<li class="sub-item"><a href="/${navData.course}/${unit.slug}/${lesson.slug}">${lesson.prefix}: ${lesson.title}</a></li>`
         }
       })
     }
