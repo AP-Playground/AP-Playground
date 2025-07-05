a36cd3d1da1e8eac26863f035e68c4e8ddb90802
apstudy <143913641+apstudy@users.noreply.github.com>
2025-07-05 01:30:16 -0700

    Fix nav-bar loading issue

--- wip/side-nav.js
+++ wip/side-nav.js
@@ -3,11 +3,13 @@ const html = document.documentElement;
 let sideNavStatus = localStorage.getItem("sideNavStatus") || "open";
 if (sideNavStatus === "closed") html.classList.add("side-nav-closed");
 
-requestAnimationFrame(function() {
+window.addEventListener("DOMContentLoaded", function() {
+  requestAnimationFrame(function() {
+    document.documentElement.classList.remove("no-transition")
+  })
   document.querySelector(".side-nav-btn").addEventListener("click", () => {
     html.classList.toggle('side-nav-closed');
     sideNavStatus = sideNavStatus === 'open' ? 'closed' : 'open';
     localStorage.setItem('sideNavStatus', sideNavStatus);
   })
-  document.documentElement.classList.remove("no-transition")
 })
