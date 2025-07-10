fa2842ca0500647c8cfca976e4e94c16c4a2a064
apstudy <143913641+apstudy@users.noreply.github.com>
2025-07-10 15:07:44 -0700

    Update side-nav.js

--- src/js/side-nav.js
+++ src/js/side-nav.js
@@ -1,11 +1,14 @@
 const html = document.documentElement;
 
 let sideNavStatus = localStorage.getItem("sideNavStatus") || "open";
+
+if (this.window.innerWidth <= 1200) sideNavStatus = 'closed';
+
 if (sideNavStatus === "closed") html.classList.add("side-nav-closed");
 
 window.addEventListener("DOMContentLoaded", function() {
   requestAnimationFrame(function() {
-    document.documentElement.classList.remove("no-transition")
+    document.documentElement.classList.remove("no-transition");
   })
   document.querySelector(".side-nav-btn").addEventListener("click", () => {
     html.classList.toggle('side-nav-closed');
