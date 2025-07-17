064bb54e62cc5297e5ce030d40bbd7de81c72c8c
apstudy <143913641+apstudy@users.noreply.github.com>
2025-07-16 23:28:54 -0700

    Temporarily added 1.7 for testing

--- scripts/generate.js
+++ scripts/generate.js
@@ -20,6 +20,7 @@ const pages = [
   'ap-biology/unit-1/lesson-4.html',
   'ap-biology/unit-1/lesson-5.html',
   'ap-biology/unit-1/lesson-6.html',
+  'ap-biology/unit-1/lesson-7.html',
   'ap-biology/unit-2.html',
 ];
 
