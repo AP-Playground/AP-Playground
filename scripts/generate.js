ffcf7812ac34435db72653b3589e016a006bb9a1
apstudy <143913641+apstudy@users.noreply.github.com>
2025-07-20 21:04:01 -0700

    Update generate.js

--- scripts/generate.js
+++ scripts/generate.js
@@ -13,7 +13,7 @@ async function fetchData(url) {
     return await response.text();
 }
 
-let examDates = cheerio.load(await fetchData("https://apcentral.collegeboard.org/exam-administration-ordering-scores/exam-dates"));
+let examDates = cheerio.load(await fetchData("https://apcentral.collegeboard.org/exam-administration-ordering-scores/exam-dates")).findAll('table.cb-table').html();
 console.log(examDates);
 
 
