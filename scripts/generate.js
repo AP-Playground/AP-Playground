ea479153cfc63273fa7a32c2d50fbfd43234a65c
AP Playground <143913641+AP-Playground@users.noreply.github.com>
2025-07-23 21:32:00 -0700

    Published AP Bio 2.3-2.7

--- scripts/generate.js
+++ scripts/generate.js
@@ -69,6 +69,11 @@ const pages = [
   'ap-biology/unit-2/index.json',
   'ap-biology/unit-2/lesson-1.json',
   'ap-biology/unit-2/lesson-2.json',
+  'ap-biology/unit-2/lesson-3.json',
+  'ap-biology/unit-2/lesson-4.json',
+  'ap-biology/unit-2/lesson-5.json',
+  'ap-biology/unit-2/lesson-6.json',
+  'ap-biology/unit-2/lesson-7.json'
 ];
 
 // write each page
