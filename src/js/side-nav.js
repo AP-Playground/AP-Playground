f2fe885b9f1afb43dde81b8adab7bb44bd875603
AP Playground <143913641+AP-Playground@users.noreply.github.com>
2025-07-23 21:41:17 -0700

    Update side-nav.js

--- src/js/side-nav.js
+++ src/js/side-nav.js
@@ -4,16 +4,9 @@ let sideNavStatus = localStorage.getItem("sideNavStatus") || "open";
 
 if (sideNavStatus === "closed") html.classList.add("side-nav-closed");
 
-
-window.addEventListener("load", function() {
-  document.documentElement.classList.remove("no-transition");
+window.addEventListener("DOMContentLoaded", function() {
   requestAnimationFrame(function() {
-    if (this.window.innerWidth <= 1200) {
-      sideNavStatus = 'closed';
-      localStorage.setItem('sideNavStatus', sideNavStatus);
-      html.classList.add('side-nav-closed');
-    }
-
+    document.documentElement.classList.remove("no-transition");
     document.querySelector(".side-nav-btn").addEventListener("click", () => {
       html.classList.toggle('side-nav-closed');
       sideNavStatus = sideNavStatus === 'open' ? 'closed' : 'open';
