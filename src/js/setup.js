2e2e23bb5e144695c5bab3a9737ab21ab496d210
AP Playground <143913641+AP-Playground@users.noreply.github.com>
2025-07-24 17:18:01 -0700

    Adding dark mode toggle to pages

---
+++ src/js/setup.js
@@ -0,0 +1,41 @@
+// Setup script for side-nav functionality
+const html = document.documentElement;
+
+let sideNavStatus = localStorage.getItem("sideNavStatus");
+if (!sideNavStatus) {
+  sideNavStatus = "open";
+  localStorage.setItem("sideNavStatus", sideNavStatus);
+}
+
+if (sideNavStatus === "closed") html.classList.add("side-nav-closed");
+
+window.addEventListener("DOMContentLoaded", function() {
+  document.documentElement.classList.remove("no-transition");
+  if (this.window.innerWidth <= 1200) {
+    sideNavStatus = 'closed';
+    localStorage.setItem('sideNavStatus', sideNavStatus);
+    html.classList.add('side-nav-closed');
+  }
+  document.querySelector(".side-nav-btn").addEventListener("click", () => {
+    html.classList.toggle('side-nav-closed');
+    sideNavStatus = sideNavStatus === 'open' ? 'closed' : 'open';
+    localStorage.setItem('sideNavStatus', sideNavStatus);
+  })
+})
+
+
+// Setup script for dark mode functionality
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+let darkModeStatus = localStorage.getItem("darkModeStatus");
+if (!darkModeStatus) {
+  darkModeStatus = prefersDarkMode ? "dark" : "light";
+  localStorage.setItem("darkModeStatus", darkModeStatus);
+}
+
+if (darkModeStatus === "dark") html.classList.add("dark-mode");
+
+document.querySelector(".dark-mode-btn").addEventListener("click", () => {
+  html.classList.toggle("dark-mode");
+  darkModeStatus = darkModeStatus === "dark" ? "light" : "dark";
+  localStorage.setItem("darkModeStatus", darkModeStatus);
+});
