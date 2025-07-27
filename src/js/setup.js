69be6e50cf73d8ea83e8ec7d26983bb967699590
AP Playground <143913641+AP-Playground@users.noreply.github.com>
2025-07-27 01:26:44 -0700

    Update setup.js

--- src/js/setup.js
+++ src/js/setup.js
@@ -5,6 +5,8 @@ if (!sideNavStatus) {
   sideNavStatus = "open";
   localStorage.setItem("sideNavStatus", sideNavStatus);
 }
+let currentSideNavScroll = 0;
+let sideNavLinks;
 
 if (sideNavStatus === "closed") html.classList.add("side-nav-closed");
 
@@ -20,6 +22,7 @@ if (!darkModeStatus) {
 if (darkModeStatus === "dark") html.classList.add("dark-mode");
 
 
+
 window.addEventListener("DOMContentLoaded", function() {
   document.documentElement.classList.remove("no-transition");
   if (this.window.innerWidth <= 1200) {
@@ -27,10 +30,17 @@ window.addEventListener("DOMContentLoaded", function() {
     localStorage.setItem('sideNavStatus', sideNavStatus);
     html.classList.add('side-nav-closed');
   }
+  sideNavLinks = document.querySelector(".side-nav-links")
   document.querySelector(".side-nav-btn").addEventListener("click", () => {
+    if (sideNavStatus === 'open') {
+      currentSideNavScroll = sideNavLinks.scrollTop;
+    }
     html.classList.toggle('side-nav-closed');
     sideNavStatus = sideNavStatus === 'open' ? 'closed' : 'open';
     localStorage.setItem('sideNavStatus', sideNavStatus);
+    if (sideNavStatus === 'open') {
+      sideNavLinks.scrollTo({ top: currentSideNavScroll, behavior: "auto" });
+    }
   })
 
   document.querySelector(".dark-mode-btn").addEventListener("click", () => {
