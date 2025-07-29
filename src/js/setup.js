1375665bd5ebf7ee93acca4a7210cc92e2b8e280
AP Playground <143913641+AP-Playground@users.noreply.github.com>
2025-07-29 01:52:15 -0700

    Update setup.js

--- src/js/setup.js
+++ src/js/setup.js
@@ -24,28 +24,30 @@ if (darkModeStatus === "dark") html.classList.add("dark-mode");
 
 
 window.addEventListener("DOMContentLoaded", function() {
-  document.documentElement.classList.remove("no-transition");
-  if (this.window.innerWidth <= 1200) {
-    sideNavStatus = 'closed';
-    localStorage.setItem('sideNavStatus', sideNavStatus);
-    html.classList.add('side-nav-closed');
-  }
-  sideNavLinks = document.querySelector(".side-nav-links")
-  document.querySelector(".side-nav-btn").addEventListener("click", () => {
-    if (sideNavStatus === 'open') {
-      currentSideNavScroll = sideNavLinks.scrollTop;
+  requestAnimationFrame(function() {
+    document.documentElement.classList.remove("no-transition");
+    if (this.window.innerWidth <= 1200) {
+      sideNavStatus = 'closed';
+      localStorage.setItem('sideNavStatus', sideNavStatus);
+      html.classList.add('side-nav-closed');
     }
-    html.classList.toggle('side-nav-closed');
-    sideNavStatus = sideNavStatus === 'open' ? 'closed' : 'open';
-    localStorage.setItem('sideNavStatus', sideNavStatus);
-    if (sideNavStatus === 'open') {
-      sideNavLinks.scrollTo({ top: currentSideNavScroll, behavior: "auto" });
-    }
-  })
+    sideNavLinks = document.querySelector(".side-nav-links")
+    document.querySelector(".side-nav-btn").addEventListener("click", () => {
+      if (sideNavStatus === 'open') {
+        currentSideNavScroll = sideNavLinks.scrollTop;
+      }
+      html.classList.toggle('side-nav-closed');
+      sideNavStatus = sideNavStatus === 'open' ? 'closed' : 'open';
+      localStorage.setItem('sideNavStatus', sideNavStatus);
+      if (sideNavStatus === 'open') {
+        sideNavLinks.scrollTo({ top: currentSideNavScroll, behavior: "auto" });
+      }
+    })
 
-  document.querySelector(".dark-mode-btn").addEventListener("click", () => {
-    html.classList.toggle("dark-mode");
-    darkModeStatus = darkModeStatus === "dark" ? "light" : "dark";
-    localStorage.setItem("darkModeStatus", darkModeStatus);
-  });
+    document.querySelector(".dark-mode-btn").addEventListener("click", () => {
+      html.classList.toggle("dark-mode");
+      darkModeStatus = darkModeStatus === "dark" ? "light" : "dark";
+      localStorage.setItem("darkModeStatus", darkModeStatus);
+    });
+  })
 })
