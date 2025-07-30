ad13869975f14a46f185902d1d497f83fea4828f
AP Playground <143913641+AP-Playground@users.noreply.github.com>
2025-07-30 01:10:25 -0700

    Creating courses page

---
+++ src/js/courses.js
@@ -0,0 +1,24 @@
+const courseBlocks = document.querySelectorAll(".courses-container > .content-block")
+const endBlock = courseBlocks.pop()
+let courses = [];
+courseBlocks.forEach(course => {
+  courses.push(course.querySelector("h2").textContent.slice(0,-1))
+})
+
+document.getElementById("course-search").addEventListener("change", (e) => {
+  const search = e.target.value;
+  let found = false;
+  courses.forEach((course, idx) => {
+    if (course.contains(search)) {
+      courseBlocks[idx].classList.remove("hidden");
+      found = true;
+    } else {
+      courseBlocks[idx].classList.add("hidden")
+    }
+  })
+  if (found) {
+    endBlock.classList.add("hidden");
+  } else {
+    endBlock.classList.remove("hidden")
+  }
+})
