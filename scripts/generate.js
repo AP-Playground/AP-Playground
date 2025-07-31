7f3b395a0770254af6d3876266f7bc0541f6611f
AP Playground <143913641+AP-Playground@users.noreply.github.com>
2025-07-30 17:25:26 -0700

    Update generate.js

--- scripts/generate.js
+++ scripts/generate.js
@@ -83,7 +83,7 @@ let coursePageList = "";
 courses.forEach(({title, slug}) => {
   coursePageList += `<div class="content-block">`
     coursePageList += `<h2>${title}:</h2>`
-    coursePageList += `<p>${JSON.parse(readFileSync("src/" + slug + "/index.html", "utf-8")).summary}</p>`
+    coursePageList += `<p>${JSON.parse(readFileSync("src/" + slug + "/index.json", "utf-8")).summary}</p>`
     coursePageList += `<p><a href="/${slug}">Course page</a></p>`
   coursePageList += `</div>`
 })
