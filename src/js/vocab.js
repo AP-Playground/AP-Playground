409444637c83543910383e5ba16244c3e0e23a63
AP Playground <143913641+AP-Playground@users.noreply.github.com>
2025-08-30 19:07:21 -0700

    Update vocab.js

--- src/js/vocab.js
+++ src/js/vocab.js
@@ -1,5 +1,5 @@
 const vocabCards = document.querySelectorAll(".vocab-card");
-const vocabCardImages = document.querySelectorAll(".vocab-card > img");
+const vocabCardImages = document.querySelectorAll(".vocab-card img");
 
 vocabCardImages.forEach(img => {
   const imgEnlarged = document.querySelector(".img-enlarged-container > img");
