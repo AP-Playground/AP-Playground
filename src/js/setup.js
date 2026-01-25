9cf9ddb7507975659338e6a29e96320e39cfb2a9
AP-Playground <143913641+AP-Playground@users.noreply.github.com>
2026-01-24 17:25:55 -0800

    .

--- src/js/setup.js
+++ src/js/setup.js
@@ -111,7 +111,9 @@ function toggleSideNav() {
 }
 
 window.addEventListener("resize", () => {
-  pageWrapper.inert = window.innerWidth <= 1200 && sideNavStatus === "open"
+  pageWrapper.inert = window.innerWidth <= 1200 && sideNavStatus === "open";
+  document.documentElement.style.setProperty('--vh', window.visualViewport.height + "px")
+  breadcrumbs.innerHTML = window.visualViewport.height;
 })
 
 const breadcrumbs = document.querySelector('.breadcrumbs');
